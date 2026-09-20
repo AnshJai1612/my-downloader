1b579dd3e1dcaa0b77af1e52172b1d3e56364d76
AnshJai1612 <157519263+AnshJai1612@users.noreply.github.com>
2026-09-20 21:28:12 +0400

    app.py

--- app.py
+++ app.py
@@ -156,5 +156,6 @@ def download_batch():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
+    import os
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
